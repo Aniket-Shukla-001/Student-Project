29bd0ca419671aad48e335fd9ebb072239471450
Aniket-Shukla-001 <142597732+Aniket-Shukla-001@users.noreply.github.com>
2025-08-08 02:37:33 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,8 +21,9 @@ def get_requirements(file_path:str)->List[str]:
 setup(
 name='mlproject',
 version='0.0.1',
-author='harsh',
-author_email='harsh@gmail.com',
+author='Aniket',
+author_email='aniketshuklaishere@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
+
 )
